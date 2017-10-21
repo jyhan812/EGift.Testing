d597b08f030e8f8f923959c0e4ff4901d0cf89fa
jyhan812 <32969446+jyhan812@users.noreply.github.com>
2017-10-20 23:31:52 -0400

    Add files via upload

---
+++ EGift.Testing/MatchParenthesisTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MatchParenthesis;
+
+namespace EGift.Testing
+{
+    [TestClass]
+    public class MatchParenthesisTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testing = new MatchingParenthesis();
+
+            string strTest = "39()(55555)";
+
+            bool result = testing.hasMatches(strTest);
+
+        }
+    }
+}
---
+++ MatchParenthesis/MatchingParenthesis.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchParenthesis
+{
+    public class MatchingParenthesis
+    {
+        public bool hasMatches(string str)
+        {
+            //remove characters other than parenthesis
+            char[] chars = str.ToCharArray();
+            int posChar = 0;
+            int removedCount = 0;
+            foreach (char ch in chars)
+            {
+                posChar++;
+                if (ch.ToString() != "(" && ch.ToString() != ")")
+                {
+                    str = str.Remove(posChar - removedCount - 1, 1);
+                    removedCount++;
+                }
+            }
+
+            bool hasMatch = DoWork(str);
+
+            return hasMatch;
+        }
+        
+        public bool DoWork(string str)
+        {
+            char[] pars = str.ToCharArray();
+
+            int leftpos = 0;
+            bool hasleftPar = false;
+            bool hasrightPar = false;
+            bool result = false;
+
+            foreach (char ch in pars)
+            {
+                leftpos++;
+                if (ch.ToString() == "(")
+                {
+                    hasleftPar = true;
+                    string substr = str.Substring(leftpos, str.Length - leftpos);
+
+                    int rightpos = leftpos;
+                    char[] subchars = substr.ToCharArray();
+                    foreach (char subch in subchars)
+                    {
+                        rightpos++;
+                        if (subch.ToString() == ")")
+                        {
+                            hasrightPar = true;
+                            str = str.Remove(leftpos - 1, 1);
+                            str = str.Remove(rightpos - 2, 1);
+                            break;
+                        }
+                    }
+                }
+
+                if (hasleftPar && hasrightPar)
+                {
+                    if (str.Length == 0)
+                        result = true;
+                    else
+                        return DoWork(str);
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
